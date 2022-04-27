f31e4422d0678444867a25a03fb92bc29d1b063d
repzlaw <ibitoyedavid@gmail.com>
2022-04-27 22:45:27 +0100

    created method to return customers current badge

--- app/Services/BadgeService.php
+++ app/Services/BadgeService.php
@@ -7,4 +7,15 @@ use App\Models\User;
 
 class BadgeService{
 
+    /**
+     * The customers current badge.
+     */
+    public function current_badge(User $user)
+    {
+        $current_badge =  $user->badges->sortByDesc('value')->first();
+
+        return $current_badge;
+    }
+
+
 }
