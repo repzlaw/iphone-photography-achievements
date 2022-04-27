665eda4c060d38819b42c415259341c714e10821
repzlaw <ibitoyedavid@gmail.com>
2022-04-27 21:53:12 +0100

    logic that listen to achievement event and fire unlock badge event when milestone is achieved

--- app/Listeners/UnlockAchievement.php
+++ app/Listeners/UnlockAchievement.php
@@ -2,8 +2,13 @@
 
 namespace App\Listeners;
 
-use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Models\Badge;
+use App\Models\Achievement;
+use App\Events\BadgeUnlocked;
+use App\Events\AchievementUnlocked;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class UnlockAchievement
 {
@@ -20,11 +25,34 @@ class UnlockAchievement
     /**
      * Handle the event.
      *
-     * @param  object  $event
+     * @param  \App\Events\AchievementUnlocked  $event
      * @return void
      */
-    public function handle($event)
+    public function handle(AchievementUnlocked $event)
     {
-        //
+        //get achievement
+        $achievement = Achievement::where('name', $event->achievement_name)->first();
+
+        //check if achievement exists
+        if(! $achievement){
+            return ;
+        }
+
+        //forget the cached achievement for this customer
+        Cache::forget('customer_achievements_'.$event->user->id);
+
+        //add the achievement to user achievements table
+        $event->user->achievements()->syncWithoutDetaching($achievement->id);
+
+        //get users badge
+        $badge = Badge::where('value', '<=', $event->user->achievements->count() )
+                        ->orderBy('value','desc')
+                        ->first();
+
+        //check if badge exist and fire badge unlocked event                
+        if ($badge) {
+            event(new BadgeUnlocked($badge->name, $event->user));
+        }
+
     }
 }
