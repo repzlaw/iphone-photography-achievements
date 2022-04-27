fed9413085f43fb9e5cc2fc22338d6d869db107d
repzlaw <ibitoyedavid@gmail.com>
2022-04-27 21:58:21 +0100

    logic that listens to unlock badge event and unlocks the badge earned

--- app/Listeners/UnlockBadge.php
+++ app/Listeners/UnlockBadge.php
@@ -2,8 +2,11 @@
 
 namespace App\Listeners;
 
-use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Models\Badge;
+use App\Events\BadgeUnlocked;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class UnlockBadge
 {
@@ -20,11 +23,18 @@ class UnlockBadge
     /**
      * Handle the event.
      *
-     * @param  object  $event
+     * @param  \App\Events\BadgeUnlocked  $event
      * @return void
      */
-    public function handle($event)
+    public function handle(BadgeUnlocked $event)
     {
-        //
+        //get badge details
+        $badge = Badge::where('name', $event->badge_name)->firstOrFail();
+
+        //add batch to user batches table for the customer
+        $event->user->badges()->syncWithoutDetaching($badge->id);
+
+        //forget cache if present
+        Cache::forget('customer_achievements_'.$event->user->id);
     }
 }
