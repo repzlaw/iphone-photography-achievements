0a9257e1cf9a55b7274c4c9d028f2a11a492b335
repzlaw <ibitoyedavid@gmail.com>
2022-04-27 21:49:10 +0100

    logic that listen to written comment event and fire achievement event when necessary

--- app/Listeners/WrittenComment.php
+++ app/Listeners/WrittenComment.php
@@ -2,8 +2,11 @@
 
 namespace App\Listeners;
 
-use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Models\Achievement;
+use App\Events\CommentWritten;
+use App\Events\AchievementUnlocked;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class WrittenComment
 {
@@ -20,11 +23,20 @@ class WrittenComment
     /**
      * Handle the event.
      *
-     * @param  object  $event
+     * @param  \App\Events\CommentWritten  $event
      * @return void
      */
-    public function handle($event)
+    public function handle(CommentWritten $event)
     {
-        //
+        //get comments
+        $comment_count = $event->comment->user->refresh()->comments->count();
+
+        //get achievement based on the comment count
+        $achievement = Achievement::where(['type'=> 'comment_written', 'value'=> $comment_count])->first();
+        
+        //check if achievement exists and fire AchievementUnlocked event
+        if ($achievement) {
+            event(new AchievementUnlocked($achievement->name, $event->comment->user));
+        }
     }
 }
