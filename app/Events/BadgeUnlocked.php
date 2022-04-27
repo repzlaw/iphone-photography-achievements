597d2b025b50feba0d18f64e2884d565b1404d98
repzlaw <ibitoyedavid@gmail.com>
2022-04-27 21:16:23 +0100

    create badge unlocked event

---
+++ app/Events/BadgeUnlocked.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class BadgeUnlocked
+{
+    use Dispatchable, SerializesModels;
+
+    public $badge_name;
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(string $badge_name, User $user)
+    {
+        $this->badge_name = $badge_name;
+        $this->user = $user;
+    }
+
+}
