2f87cc148b698c43f77247751e9cb5a5f2e17fe2
repzlaw <ibitoyedavid@gmail.com>
2022-04-27 22:42:03 +0100

    method to return next available achievements

--- app/Services/AchievementService.php
+++ app/Services/AchievementService.php
@@ -17,4 +17,39 @@ class AchievementService{
         return $unlocked_achievements;
     }
 
+    /**
+     * get the next available achievements for the customer.
+     */
+    public function next_available_achievements(User $user)
+    {
+        $next_achievements = [];
+
+        // Get watched lessons achievements
+        $watched_lessons = $user->watched->count();
+
+        $get_achievement = Achievement::where([
+            'type'=>'lesson_watched',
+            ['value', '>', $watched_lessons]])
+            ->orderBy('value', 'asc')
+            ->first();
+
+        if ($get_achievement) {
+            $next_achievements[] = $get_achievement->name;
+        }
+
+        // Get written comment achievements
+        $written_comments = $user->comments->count();
+
+        $get_achievement = Achievement::where([
+            'type'=>'comment_written',
+            ['value', '>', $written_comments]])
+            ->orderBy('value', 'asc')
+            ->first();
+
+        if ($get_achievement) {
+            $next_achievements[] = $get_achievement->name;
+        }
+        
+        return $next_achievements;
+    }
 }
